7c9eb71df2be5264f58409a87b6d4bcd3bd3d86b
ToniYenC11 <tonicastanares11@gmail.com>
2025-09-16 10:16:09 +0800

    Added placeholder text files to Datasets_md

--- src/fill.py
+++ src/fill.py
@@ -1,7 +1,9 @@
+# This is a simpel function to add placeholder text files to each directory in Datasets 
+
 import os
 import subprocess
 
-for root, dirs, files in os.walk('Datasets') :
+for root, dirs, files in os.walk('Datasets_md') :
     for fir in dirs:
         file_name = f"{root}/{fir}/{fir}.txt"
         print(file_name)
